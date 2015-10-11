f8c32a8741a50574a8467eb22e600da15ce30efb
ÁéãÊÖßÂãá <weifeng19911024@163.com>
2015-10-11 15:53:44 +0800

    Initial Commit

---
+++ FractionCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FractionCalculator
+//
+//  Created by wanghuiyong on 10/11/15.
+//  Copyright © 2015 wanghuiyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FractionCalculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FractionCalculator
+//
+//  Created by wanghuiyong on 10/11/15.
+//  Copyright © 2015 wanghuiyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
